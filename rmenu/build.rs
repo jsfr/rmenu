99a0b421c99ad90ce6d95df8eb6f59179eb6aad1
Jens Fredskov <jsfr@users.noreply.github.com>
2021-02-02 15:04:01 +0100

    Add tests + build completions for bash and zsh

--- rmenu/build.rs
+++ rmenu/build.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use clap::IntoApp;
-use clap_generate::{generate_to, generators::Fish};
+use clap_generate::{generate_to, generators::{Fish, Zsh, Bash}};
 
 include!("src/cli.rs");
 
@@ -9,5 +9,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let mut app = Cli::into_app().bin_name("rmenu");
 
-    generate_to::<Fish, _, _>(&mut app, "rmenu", out_dir);
+    generate_to::<Fish, _, _>(&mut app, "rmenu", &out_dir);
+    generate_to::<Bash, _, _>(&mut app, "rmenu", &out_dir);
+    generate_to::<Zsh, _, _>(&mut app, "rmenu", &out_dir);
 }
--- rmenu_history/build.rs
+++ rmenu_history/build.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use clap::IntoApp;
-use clap_generate::{generate_to, generators::Fish};
+use clap_generate::{generate_to, generators::{Fish, Zsh, Bash}};
 
 include!("src/cli.rs");
 
@@ -9,5 +9,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let mut app = Cli::into_app().bin_name("rmenu_history");
 
-    generate_to::<Fish, _, _>(&mut app, "rmenu_history", out_dir);
+    generate_to::<Fish, _, _>(&mut app, "rmenu_history", &out_dir);
+    generate_to::<Bash, _, _>(&mut app, "rmenu_history", &out_dir);
+    generate_to::<Zsh, _, _>(&mut app, "rmenu_history", &out_dir);
 }
