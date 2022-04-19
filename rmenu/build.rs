0f5e63183222581825cbcd5e7041211441d367c6
Jens Fredskov <jsfr@users.noreply.github.com>
2022-04-19 15:49:41 +0200

    Upgrade dependencies

--- rmenu/build.rs
+++ rmenu/build.rs
@@ -8,7 +8,7 @@ include!("src/cli.rs");
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let app_name = "rmenu";
-    let mut app = Cli::into_app().bin_name(app_name);
+    let mut app = Cli::command().bin_name(app_name);
 
     generate_to(Fish, &mut app, app_name, &out_dir).expect("Failed to generate fish completions");
     generate_to(Zsh, &mut app, app_name, &out_dir).expect("Failed to generate zsh completions");
